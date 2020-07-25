887e1e2d7c1f31dad5804a651947c4a8d5b912d8
Sam <sam_ring@hotmail.com>
2020-07-26 00:50:31 +0100

    Updates

---
+++ precache-manifest.9ab74e870b8a3018db8e73fc3090ade6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52dce9fefe84cbe2e3f63034ae46a62f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "326cbd69333f4597cb05",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3d4baf25199a43b83fb4",
+    "url": "/static/css/main.bc178f93.chunk.css"
+  },
+  {
+    "revision": "326cbd69333f4597cb05",
+    "url": "/static/js/2.787a2c8d.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/static/js/2.787a2c8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d4baf25199a43b83fb4",
+    "url": "/static/js/main.d62114c7.chunk.js"
+  },
+  {
+    "revision": "22c73ef20c632b424314",
+    "url": "/static/js/runtime-main.75c5cf27.js"
+  },
+  {
+    "revision": "0b4325672bc5cc64772ac0ee94997bb2",
+    "url": "/static/media/Scoreboard_mvp.0b432567.webp"
+  },
+  {
+    "revision": "b7df8531d2219274cc1a654035957123",
+    "url": "/static/media/cs-logo-inv.b7df8531.png"
+  }
+]);

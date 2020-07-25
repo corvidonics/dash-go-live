e01b721e4c1abb90623ed181f006766c58fe8fe9
Sam <sam_ring@hotmail.com>
2020-07-26 00:18:54 +0100

    Updates

---
+++ precache-manifest.654916147f610bccaf33bf77ce47e74c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8039e783191b7c8031af65c53d24ecd",
+    "url": "/dash-go-live/index.html"
+  },
+  {
+    "revision": "326cbd69333f4597cb05",
+    "url": "/dash-go-live/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0539f179138425184906",
+    "url": "/dash-go-live/static/css/main.bc178f93.chunk.css"
+  },
+  {
+    "revision": "326cbd69333f4597cb05",
+    "url": "/dash-go-live/static/js/2.787a2c8d.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/dash-go-live/static/js/2.787a2c8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0539f179138425184906",
+    "url": "/dash-go-live/static/js/main.d726ce33.chunk.js"
+  },
+  {
+    "revision": "3b53bc070f64ac8e7d3e",
+    "url": "/dash-go-live/static/js/runtime-main.2c4671b0.js"
+  },
+  {
+    "revision": "0b4325672bc5cc64772ac0ee94997bb2",
+    "url": "/dash-go-live/static/media/Scoreboard_mvp.0b432567.webp"
+  },
+  {
+    "revision": "b7df8531d2219274cc1a654035957123",
+    "url": "/dash-go-live/static/media/cs-logo-inv.b7df8531.png"
+  }
+]);
